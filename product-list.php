9d2f910f291efbc6e3e40db3fda218ebb16db677
Keith Stedman <keithstedman@gmail.com>
2017-03-17 20:35:16 -0700

    added

--- product-list.php
+++ product-list.php
@@ -2,7 +2,7 @@
   include_once "php/db-connect.php";
 
   $produstListSql = "SELECT * FROM `product-list`;";
-  $resultArray = [];
+  $resultArray = array();
   $i = 0;
   $res = $conn->query($produstListSql);
   while($row = $res->fetch_assoc()) {
