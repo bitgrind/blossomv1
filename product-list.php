4f7ccf13e673470e024683b507a0b9a093e7ca62
Keith Stedman <keithstedman@gmail.com>
2017-03-17 19:46:47 -0700

    added

--- product-list.php
+++ product-list.php
@@ -3,10 +3,7 @@
 
   $produstListSql = "SELECT * FROM `product-list`;";
 
-  /* Prepare statement */
-  if($res = $conn->query($produstListSql)){
-    //print_r($res);
-  }
+  $res = $conn->query($produstListSql));
   $conn->close();
  ?>
  <!DOCTYPE html>
@@ -21,15 +18,13 @@
      <main>
        <div class="menu-list">
          <?php
-         //product-name | product-cost | product-catagroy | product-desc
            while($row = $res->fetch_assoc()) {
              echo "<div class='menu-item'><h1>".$row['product-catagroy']."</h1>";
              echo "<h2>".$row['product-name']."</h2>";
              echo "<h2 class='product-cost'>".$row['product-cost']."</h2>";
              echo "<p class='product-desc'>".$row['product-desc']."</p>";
              echo "</div>";
-           }
-         ?>
+           } ?>
        </div>
      </main>
    </body>
