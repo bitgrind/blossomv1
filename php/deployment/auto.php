43789f7055e3d93525bcedda3bc9775147edd6e5
Keith Stedman <keithstedman@gmail.com>
2017-03-17 17:12:04 -0700

    html architecture

--- php/deployment/auto.php
+++ php/deployment/auto.php
@@ -8,13 +8,3 @@
    shell_exec("./pull.sh");
  }
 ?>
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <title></title>
-  </head>
-  <body>
-    <h1>Auto Deployment</h1>
-  </body>
-</html>
