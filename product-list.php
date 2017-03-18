b76d485aa7c3aba68973605f3ec422cb0db85dae
Keith Stedman <keithstedman@gmail.com>
2017-03-17 19:42:57 -0700

    added

--- product-list.php
+++ product-list.php
@@ -14,19 +14,24 @@
  <html>
    <head>
      <meta charset="utf-8">
+     <link rel="stylesheet" href="/css/home.css">
      <title>Product list</title>
    </head>
    <body>
      <h1>Product List</h1>
-     <?php
-     //product-name | product-cost | product-catagroy | product-desc
-       while($row = $res->fetch_assoc()) {
-         echo "<div class='menu'><h1>".$row['product-catagroy']."</h1>";
-         echo "<h2>".$row['product-name']."</h2>";
-         echo "<h2 class='product-cost'>".$row['product-cost']."</h2>";
-         echo "<p class='product-desc'>".$row['product-desc']."</p>";
-         echo "</div>";
-       }
-     ?>
+     <main>
+       <div class="product-list">
+         <?php
+         //product-name | product-cost | product-catagroy | product-desc
+           while($row = $res->fetch_assoc()) {
+             echo "<div class='menu'><h1>".$row['product-catagroy']."</h1>";
+             echo "<h2>".$row['product-name']."</h2>";
+             echo "<h2 class='product-cost'>".$row['product-cost']."</h2>";
+             echo "<p class='product-desc'>".$row['product-desc']."</p>";
+             echo "</div>";
+           }
+         ?>
+       </div>
+     </main>
    </body>
  </html>
