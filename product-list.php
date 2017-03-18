1c34f7f6e9df4e3626290ced79bfaa99ce728d4d
Keith Stedman <keithstedman@gmail.com>
2017-03-17 19:33:55 -0700

    added

--- product-list.php
+++ product-list.php
@@ -19,9 +19,13 @@
    <body>
      <h1>Product List</h1>
      <?php
+     //product-name | product-cost | product-catagroy | product-desc
        while($row = $res->fetch_assoc()) {
-         print_r($row);
-         echo "<br>";
+         echo "<div class='menu'><h1>".$row['product-catagroy']."</h1>";
+         echo "<h2>".$row['product-name']."</h2>";
+         echo "<h2 class='product-cost'>".$row['product-cost']."</h2>";
+         echo "<p class='product-desc'>".$row['product-desc']."</p>";
+         echo "</div>";
        }
      ?>
    </body>
